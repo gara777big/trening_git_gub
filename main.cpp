cd039a9dbc1c6d50c2eb8379a19808925aee7e58
garar <gara.radmir2017@yandex.ru>
2022-08-26 09:57:36 +0500

    landush

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     int a = 45;
     int b = a * 9;
+    for(int i = 0; i < 20; ++i)
+    {
+        std::cout << "h" << std::endl;
+    }
     return 0;
 }
